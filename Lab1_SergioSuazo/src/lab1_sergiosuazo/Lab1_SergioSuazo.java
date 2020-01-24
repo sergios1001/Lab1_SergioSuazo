b8baf4fdfc9884993a44a58b9f96374153fbe486
Sergio Suazo <sergiosb1001@gmail.com>
2020-01-25 05:07:54 +0800

    Hanoi

--- Lab1_SergioSuazo/src/lab1_sergiosuazo/Lab1_SergioSuazo.java
+++ Lab1_SergioSuazo/src/lab1_sergiosuazo/Lab1_SergioSuazo.java
@@ -1,21 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab1_sergiosuazo;
 
-/**
- *
- * @author Sergio
- */
+import java.util.Scanner;
+
 public class Lab1_SergioSuazo {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner leer=new Scanner(System.in);
     public static void main(String[] args) {
-        // TODO code application logic here
+        char resp='s';
+        while(resp=='s'||resp=='S')
+        {
+            System.out.println("Seleccione el ejercicio que quiere probar:\n"
+                    + "1: Torre de Hanoi\n"
+                    + "2: Diagonales\n"
+                    + "3: Fechas\n");
+            int n;
+            n=leer.nextInt();
+            switch(n)
+            {
+                case 1:
+                {
+                    System.out.print("Numero de discos: ");
+                    int disc,paso=0;
+                    disc=leer.nextInt();
+                    Hanoi(disc,1,2,3);
+                    
+                }
+                case 2:
+                {
+                
+                }
+                case 3:
+                {
+                
+                }   
+            }
+            System.out.println("Quiere escoger un nuevo ejercicio? (s/n)");
+        }
+    }
+    
+    public static void Hanoi(int disc,int origen,int aux,int destino)
+    {
+        if(disc==1)
+        {
+            System.out.println(" mover disco "+disc+" de "+origen+" a "+destino);
+        }
+        else
+        {
+            Hanoi(disc-1,origen,destino,aux);
+            System.out.println(" mover disco "+disc+" de "+origen+" a "+destino);
+            Hanoi(disc-1,aux,origen,destino);
+        }
     }
     
 }
