27ec9d7064487d41d8af209778cd545831ccd1fe
Sergio Suazo <sergiosb1001@gmail.com>
2020-01-25 05:51:35 +0800

    diagonales

--- Lab1_SergioSuazo/src/lab1_sergiosuazo/Lab1_SergioSuazo.java
+++ Lab1_SergioSuazo/src/lab1_sergiosuazo/Lab1_SergioSuazo.java
@@ -2,10 +2,12 @@
 package lab1_sergiosuazo;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Lab1_SergioSuazo {
 
     static Scanner leer=new Scanner(System.in);
+    static Random rand=new Random();
     public static void main(String[] args) {
         char resp='s';
         while(resp=='s'||resp=='S')
@@ -28,7 +30,15 @@ public class Lab1_SergioSuazo {
                 }
                 case 2:
                 {
-                
+                    int size;
+                    int[][]mat;
+                    System.out.print("Ingrese tamaño de la matriz: ");
+                    size=leer.nextInt();
+                    mat=fillMatrix(size,size);
+                    System.out.println("Matriz creada: ");
+                    printMatrix(mat);
+                    Diagonales(mat,0,0);
+                    
                 }
                 case 3:
                 {
@@ -53,4 +63,54 @@ public class Lab1_SergioSuazo {
         }
     }
     
+    public static int[][] fillMatrix(int fila, int columna)
+    {
+        int[][]temp=new int[fila][columna];
+        for(int i=0;i<fila;i++)
+        {
+            for(int j=0;j<columna;j++)
+            {
+                temp[i][j]=1+rand.nextInt(10);
+            }
+        }
+        return temp;
+    }
+    
+    public static void printMatrix(int[][]matrix)
+    {
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix[0].length;j++)
+            {
+                System.out.print("["+matrix[i][j]+"]");
+            }
+            System.out.println();
+        }
+    }
+    public static void Diagonales(int[][]mat,int i, int j)
+    {
+        if(i==mat.length-1&&j==mat.length-1)
+        {
+            System.out.print(mat[i][j]);
+        }
+        else
+        {
+            if(i==j)
+            {
+                System.out.print(mat[i][j]);
+            }
+            else if(i+j==mat.length-1)
+            {
+                System.out.print(mat[i][j]);
+            }
+            if(i==mat.length-1)
+            {
+                Diagonales(mat, 0, j+1);
+            }
+            else
+            {
+                Diagonales(mat, i+1, j);
+            }
+        }
+    }
 }
